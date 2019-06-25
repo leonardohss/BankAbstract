a4935b97123ce7bac1a12d841c84e34b485c6a2d
Leonardo Sousa <leorick97@gmail.com>
2019-06-24 22:28:46 -0300

    Implementation class's and program

---
+++ BankAbstract/Entities/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAbstract.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            //metodo sobrescrito, recebe o profixo OVERRIDE
+            base.Withdraw(amount);//base reaproveita o funcionamento do metodo original
+            Balance -= 2.0;
+        }
+    }
+}
--- BankAbstract/Program.cs
+++ BankAbstract/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using BankAbstract.Entities;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace BankAbstract
 {
@@ -6,7 +9,30 @@ namespace BankAbstract
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Account> list = new List<Account>();
+
+            list.Add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+            list.Add(new BusinessAccount(1002, "Maria", 500.0, 400));
+            list.Add(new SavingsAccount(1003, "Bob", 500.0, 0.01));
+            list.Add(new BusinessAccount(1004, "Ana", 500.0, 500.0));
+
+            double sum = 0.0;
+            foreach(Account x in list)
+            {
+                sum += x.Balance;
+            }
+            Console.WriteLine("Total Balance: " + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+            foreach(Account y in list)
+            {
+                y.Withdraw(10.0);
+            }
+
+            foreach(Account z in list)
+            {
+                Console.WriteLine("Update balance for account " +
+                    z.Number + ": " + z.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
