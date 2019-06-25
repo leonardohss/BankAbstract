e4e28c7f17f4d6592ce75af1c9e963ab5fe6a146
Leonardo Sousa <leorick97@gmail.com>
2019-06-24 22:05:13 -0300

    Create the project

---
+++ BankAbstract/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankAbstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
